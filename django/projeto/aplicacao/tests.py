f4d17afe31b8d8d85386239bdad821934c964d32
octavio telles <octavio.telles@linnks.com.br>
2020-07-19 22:09:34 -0300

    tests done

--- django/projeto/aplicacao/tests.py
+++ django/projeto/aplicacao/tests.py
@@ -1,12 +1,83 @@
 import json
-from django.contrib.auth.models import User
-from django.urls import reverse
-from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from rest_framework import status
-from profiles.models import Profile
-from profiles.api.serializers import ProfileSerializer
+from django.urls import reverse
+
+from .models import Endereco
+from .serializers import EnderecoSerializer
+from .views import EnderecoView
 
 class EnderecoTestCase(APITestCase):
+    def test_post(self):
+        data = {
+            "streetName": "rua teste",
+            "number" : "teste",
+            "complement" : "teste",
+            "neighbourhood" : "teste",
+            "city" : "teste",
+            "state" : "teste",
+            "country" : "teste",
+            "zipcode" : "teste",
+            "latitude" : "teste",
+            "longitude" : "teste"
+        }
+        response = self.client.post("/endereco/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list(self):
+        response = self.client.get(reverse("endereco-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
     def test_get(self):
-        data = {}
+        response = self.client.get(reverse("endereco-list"), kwargs={"pk": 1})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_put(self):
+        data = {
+            "streetName": "rua teste",
+            "number" : "teste",
+            "complement" : "teste",
+            "neighbourhood" : "teste",
+            "city" : "teste",
+            "state" : "teste",
+            "country" : "teste",
+            "zipcode" : "teste",
+            "latitude" : "teste",
+            "longitude" : "teste"
+        }
+        post_teste = self.client.post("/endereco/", data)
+        teste_id = json.loads((post_teste.content)).get("id")
+        data = {
+            "streetName": "rua teste2",
+            "number" : "teste",
+            "complement" : "teste",
+            "neighbourhood" : "teste",
+            "city" : "teste",
+            "state" : "teste",
+            "country" : "teste",
+            "zipcode" : "teste",
+            "latitude" : "teste",
+            "longitude" : "teste"
+        }
+        response = self.client.put('/endereco/1/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete(self):
+        data = {
+            "streetName": "rua teste",
+            "number" : "teste",
+            "complement" : "teste",
+            "neighbourhood" : "teste",
+            "city" : "teste",
+            "state" : "teste",
+            "country" : "teste",
+            "zipcode" : "teste",
+            "latitude" : "teste",
+            "longitude" : "teste"
+        }
+        post_teste = self.client.post("/endereco/", data)
+        teste_id = json.loads((post_teste.content)).get("id")
+
+        response = self.client.delete('/endereco/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- readme.md
+++ readme.md
@@ -4,13 +4,21 @@ Pequeno projeto usando Docker, TDD e Django.
 
 #Como iniciar o projeto?
 O jeito mais fácil de rodar a aplicação é rodando o comando "docker-compose up", que realizará todas as instalações necessárias.
-Para rodar o comando docker-compose, é necessário instalar o Docker e o Docker Compose (que já vem com o Docker, caso você esteja usando Windows).
+Para rodar o comando docker-compose, é necessário estar no diretório "stoomapp", instalar o Docker e o Docker Compose (que já vem com o Docker, caso você esteja usando Windows).
 
 Caso não seja possível instalar o Docker, pode-se seguir os passos:
-1. Navegar até a pasta "django" com o comando "cd django";
+1. Navegar até o diretório "django" com o comando "cd django";
 2. Criar um ambiente com o comando "python -m venv ambiente";
 3. Ativar o ambiente com o comando apropriado. No cmd, basta "ambiente\Scripts\activate". No bash, "source ambiente/Scripts/activate";
 4. Instalar as dependências com o comando "pip install -r requirements.txt";
+5. Navegar até o diretório "projeto" com o comando "cd projeto";
 5. Executar o django com "python manage.py runserver"
 
-#Como parar o projeto?
+Após a inicialização da aplicação, basta acessar no navegador de preferência a URL http://127.0.0.1:8000/endereco/
+
+#Como parar?
+1. Ctrl+C no terminal para parar a aplicação;
+2. "docker-compose down" se estiver usando o Docker Compose
+
+#Executando testes?
+Com o Docker (mas sem precisar rodar o container): Executar no terminal "docker-compose run web python django/projeto/manage.py test"
